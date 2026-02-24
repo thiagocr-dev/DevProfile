c04ae8b5269f1b8bcc9f86dab72edb3b89fd77fc
thiago.dev <thiagocolombor@gmail.com>
2026-02-24 11:50:30 -0300

    Overflow cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import "./Home.css"
 import { use } from "react"
 import { useNavigate } from "react-router"
+import { useState } from "react"
 import developers from "../data/developers"
 import DeveloperCard from "../components/DeveloperCard/DeveloperCard"
 
 function Home(){
 
-    const navigate = useNavigate()
+    const [search, setSearch] = useState("")
+
+    const filteredDevelopers = developers.filter(dev => 
+                dev.name.toLowerCase().includes(search.toLowerCase()))
+
     return(
 
         <div className="home__container">
@@ -14,12 +19,25 @@ function Home(){
                 <h1 className="title">Descubre desarrolladores</h1>
                 <p className="subtitle">Explora perfiles y conecta con talento tech</p>
             </section>
+                
+            <input 
+                type="text"
+                placeholder="Buscar desarrollador..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="search-input"
+            />
 
             <div className="cards-container">
-                {developers.map(dev => (
+                {filteredDevelopers.length > 0 ? (
+                    filteredDevelopers.map((dev) => (
                     <DeveloperCard key={dev.id} developer={dev} />
-                ))}</div>
+                ))
+                ) : (
+                    <p className="no-results">No se encontraron desarrolladores</p>
+                )}
             </div>
+        </div>
     )
 }
 
