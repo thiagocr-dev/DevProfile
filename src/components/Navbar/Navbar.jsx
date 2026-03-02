cf967b7844305f8a43f33e63a060e26066bf6fac
thiago.dev <thiagocolombor@gmail.com>
2026-03-01 22:19:32 -0300

    arreglos a NavBar y layout de media queries

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import './Navbar.css'
+import { NavLink } from 'react-router'
 
 function Navbar() {
     return (
         <nav className='navbar'>
             <h2 className='navbar__logo'>DevProfiles</h2>
+            <div className='navbar__icons'>
+                <NavLink to='/' className='navbar__icon' title='Home' aria-label='Home'>🏠</NavLink>
+                <NavLink to='/dev/crear' className='navbar__icon' title='Crear perfil' aria-label='Crear perfil'>➕</NavLink>
+            </div>
         </nav>
     )
 }
