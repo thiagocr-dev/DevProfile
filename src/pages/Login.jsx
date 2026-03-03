8e30e49c66051153a9912c1016ac97290a0dd8a9
thiago.dev <thiagocolombor@gmail.com>
2026-03-03 20:28:28 -0300

    Creacion de Pagina de Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,51 @@
+import { useNavigate } from 'react-router'
+import { useAuth } from '../context/AuthContext'
 import './Login.css'
 
 function Login() {
+
+    const {login} = useAuth ()
+    const navigate = useNavigate()
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const success = login(username, password)
+
+        if (success) {
+            navigate('/')
+        } else {
+            setError('Credenciales incorrectas')
+        }
+    }
+
     return (
-        <div>Login</div>
+        <form onSubmit={handleSubmit}>
+            <h2>Login</h2>
+
+            {error && <p style={{color: "red"}}>{error}</p>}
+
+            <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button type="submit">Ingresar</button>
+        </form>
+        
     )
 }
 
